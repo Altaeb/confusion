eb76625cc1a4b9f6843367a67456759bcd2d7bc5
Altaeb <abd2010ly@gmail.com>
2020-06-28 22:54:50 +0200

    Buttons and Icons. Learn about using buttons and icons in buttons. Also learn how to respond to button clicks. *  Add buttons and icons to app. *  Respond to button clicks within app.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View, Text, ScrollView, FlatList } from 'react-native';
+import { Card, Icon } from 'react-native-elements';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -14,6 +15,14 @@ function RenderDish(props) {
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
+                    <Icon
+                        raised
+                        reverse
+                        name={ props.favorite ? 'heart' : 'heart-o'}
+                        type='font-awesome'
+                        color='#f50'
+                        onPress={() => props.favorite ? console.log('Already favorite') : props.onPress()}
+                    />
             </Card>
         );
     } else {
@@ -21,13 +30,42 @@ function RenderDish(props) {
     }
 }
 
+function RenderComments(props) {
+    const comments = props.comments;
+
+    const RenderCommentsItem = ({ item, index }) => {
+        return(
+            <View ket={index} style={{margin: 10}}>
+                <Text style={{fontSize: 14}}>{item.comment}</Text>
+                <Text style={{fontSize: 12}}>{item.rating} Stars</Text>
+                <Text style={{fontSize: 12}}>{'--' + item.author + ', ' + item.date}</Text>
+            </View>
+        );
+    }
+    return(
+        <Card title="Comments">
+            <FlatList
+                data={comments}
+                renderItem={RenderCommentsItem}
+                keyExtractor={item => item.id.toString()} 
+            />
+        </Card>
+    );
+}
+
 class DishDetail extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            favorites: []
         };
+    }
+
+    markFavorite(dishId) {
+        this.setState({ favorites: this.state.favorites.concat(dishId)})
     }
 
     static navigationOptions = {
@@ -37,7 +75,12 @@ class DishDetail extends Component {
     render() {
         const dishId = this.props.navigation.getParam('dishId', '');
         return(
-            <RenderDish dish={this.state.dishes[+dishId]} />
+            <ScrollView>
+                <RenderDish dish={this.state.dishes[+dishId]}
+                    favorite={this.state.favorites.some(el => el === dishId)}
+                    onPress={() => this.markFavorite(dishId)} />
+                <RenderComments comments={this.state.comments.filter((comment) => comment.dishId === dishId)} />
+            </ScrollView>
         );
     }
 }
