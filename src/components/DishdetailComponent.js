156f7cf946a566bf538810280420f4b356064d79
Altaeb <abd2010ly@gmail.com>
2020-06-28 14:17:13 +0200

    Navigation Part 1. We explore navigation support for our React Native application using React Navigation, providing mobile app like navigation among the views. *  Configure your React Native application with navigation support. *  Use the Stack Navigator from React Navigation to provide stack like navigation among the views. *  Enable the passing of information among views using navigation parameters.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 
 function RenderDish(props) {
     const dish = props.dish;
-
+    console.log(dish)
     if (dish != null ) {
         return(
             <Card
@@ -13,15 +14,32 @@ function RenderDish(props) {
                     <Text style={{margin: 10}}>
                         {dish.description}
                     </Text>
-                </Card>
+            </Card>
         );
     } else {
         return(<View></View>)
     }
 }
 
-function Dishdetail(props) {
-    return(<RenderDish dish={props.dish} />);
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId', '');
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
 }
 
-export default Dishdetail;
+export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,28 +1,14 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import Dishdetail from './DishdetailComponent';
-import { View } from 'react-native';
+import { View, Platform } from 'react-native';
+import MenuNavigator from '../navigation/MenuNavigator'
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state ={
-            dishes: DISHES,
-            selectedDish: null
-        }
-    }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
 
     render() {
         return(
-            <View>
-                <Menu dishes={this.state.dishes}
-                    onPress={(dishId) => this.onDishSelect(dishId)} />
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+            <View style={{flex:1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                <MenuNavigator />
             </View>
         );
     }
