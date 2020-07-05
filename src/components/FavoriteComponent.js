09120da93c9d53a9cc2c2768bedb19da73d2dcaa
Altaeb <abd2010ly@gmail.com>
2020-07-05 23:02:34 +0200

    Swipeout and Deleting Favorites. Use a swipe gesture to reveal option buttons under a list item. This will be used to support deleting a favorite. *  Use swipe gesture to reveal option buttons. *  Click on a delete option button to delete the favorite.

--- src/components/FavoriteComponent.js
+++ src/components/FavoriteComponent.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 import { FlatList, View, Text } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
+import swipeout from 'react-native-swipeout';
 
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import { deleteFavorite } from '../redux/ActionCreators';
+import Swipeout from 'react-native-swipeout';
 
 const mapStateToProps = state => {
     return {
@@ -12,6 +15,10 @@ const mapStateToProps = state => {
         favorites: state.favorites
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
 
 class Favorites extends Component {
 
@@ -23,14 +30,27 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
 
         const renderMenuItem = ({ item, index }) => {
+
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
-                    leftAvatar={{ source: { uri: baseUrl + item.image } }} />
+                <Swipeout
+                    right={rightButton}
+                    autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        hideChevron={true}
+                        onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                        leftAvatar={{ source: { uri: baseUrl + item.image } }} />
+                </Swipeout>
             );
         }
 
@@ -57,4 +77,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
