e1c46724162440d7d6ef52d0c69fec6395d2c997
Altaeb <abd2010ly@gmail.com>
2020-07-09 09:41:38 +0200

    Simple Change.

--- src/components/ReservationComponent.js
+++ src/components/ReservationComponent.js
@@ -50,7 +50,7 @@ class Reservation extends Component {
     render() {
         return (
             <ScrollView>
-                <Animatable.View animation="zoomInDown" duration={2000} delay={1000}>
+                <Animatable.View animation="zoomIn" duration={2000} delay={1000}>
                     <View style={styles.formRow}>
                         <Text style={styles.formLabel}>Number of Guests</Text>
                         <Picker
